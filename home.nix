2a21468bb1c9064a89aca36168511c5327bcacec
cte <cestreich@gmail.com>
2025-01-14 23:55:12 -0800

    Remove GTK config

--- home.nix
+++ home.nix
@@ -143,39 +143,6 @@ in {
 
   # gnome
 
-  gtk = {
-    enable = true;
-
-    # iconTheme = {
-    #   name = "Papirus-Dark";
-    #   package = pkgs.papirus-icon-theme;
-    # };
-
-    theme = {
-      name = "orchis";
-      package = pkgs.orchis-theme;
-    };
-
-    # cursorTheme = {
-    #   name = "Numix-Cursor";
-    #   package = pkgs.numix-cursor-theme;
-    # };
-
-    # gtk3.extraConfig = {
-    #   Settings = ''
-    #     gtk-application-prefer-dark-theme=1
-    #   '';
-    # };
-
-    # gtk4.extraConfig = {
-    #   Settings = ''
-    #     gtk-application-prefer-dark-theme=1
-    #   '';
-    # };
-  };
-
-  home.sessionVariables.GTK_THEME = "orchis";
-
   dconf = {
     enable = true;
 
