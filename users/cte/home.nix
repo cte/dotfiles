66219c9843acf89b47fb7e5be2adb6d14dc0469e
cte <cestreich@gmail.com>
2025-01-16 16:22:22 -0800

    Add packages module

--- users/cte/home.nix
+++ users/cte/home.nix
@@ -6,7 +6,7 @@
     ../../modules/home/config.nix
     ../../modules/home/shell.nix
     ../../modules/home/gnome.nix
-    # ../../modules/home/packages.nix
+    ../../modules/home/packages.nix
     ../../modules/home/programs.nix
     ../../modules/home/services.nix
   ];
