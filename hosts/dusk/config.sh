8eaac8e04e762bebba471de6f3f3daec3f19f006
cte <cestreich@gmail.com>
2025-01-22 22:02:50 -0800

    Fix config scripts

--- hosts/dusk/config.sh
+++ hosts/dusk/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../../config && ./install.sh \
+cd ~/dotfiles/config && ./install.sh \
   bin \
   btop \
   cava \
--- hosts/nyx/config.sh
+++ hosts/nyx/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../../config && ./install.sh \
+cd ~/dotfiles/config && ./install.sh \
   btop \
   cursor \
   kitty \
